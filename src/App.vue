9291c11894b4c061b19c22e8908fd07b1d7d2afb
kira_wang_aug <angelwang11a@outlook.com>
2023-08-16 09:38:53 +0800

    pushed lisao-ipsum code

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+  export default {
+    data(){
+      return {
+          msg: "离骚 Ipsum",
+          text: "生成一行文本",
+          paragraph: "生成多个段落"
+      }
+    }
+  }
+</script>
+
+<template>
+  <h1 class = 'h1'>{{ msg }}</h1>
+  <div>
+    <a-button type = 'primary' block class = 'button'>{{ text }}</a-button>
+    <a-button block>{{ paragraph }}</a-button>
+  </div>
+  
+</template>
+
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import { Button } from 'ant-design-vue';
+import Vue from "vue";
+Vue.use(Button);
+
+
+createApp(App).mount('#app')
