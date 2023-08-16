b9fa5e7b09f11cf5af6cc71fc3f7bed889e1c1ac
kira_wang_aug <angelwang11a@outlook.com>
2023-08-16 15:48:29 +0800

    structure changed

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script>
+  
+</script>
+
+<template>
+  <div>
+    <h1>离骚ipsum</h1>
+    <button type="submit" value = "text">生成一行文字</button>
+    <button type="submit" value = "paragraph">生成多段段落</button>
+  </div>
+</template>
+
