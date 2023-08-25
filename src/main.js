12b8b8f44b39a63bd1a6db99826a0a9362bd2368
kira_wang_aug <angelwang11a@outlook.com>
2023-08-25 16:52:39 +0800

    added router

--- src/main.js
+++ src/main.js
@@ -2,5 +2,21 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import * as VueRouter from 'vue-router'
 
-createApp(App).mount('#app')
+const word = {template: '<div>Word</div>'}
+const paragraph = {template: '<div>Paragraph</div>'}
+
+const routes = [
+    { path: '/word', component: word },
+    { path: '/paragraph', component: paragraph },
+]
+  
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHashHistory(),
+    routes,
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
