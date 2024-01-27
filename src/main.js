68a86f68a5733c0fbdc04a570c6e6fc60a7cffa4
kira_wang_aug <angelwang11a@outlook.com>
2024-01-27 11:42:44 +0800

    restarted

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import router from './router';
-
-new Vue({
-    router,
-    render: h=>h(App)
-}).$mount('#app');
-
-
--- src/word.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div>
-        <h2>word</h2>
-    </div>
-</template>
-
-<script>
-    export default {}
-</script>
